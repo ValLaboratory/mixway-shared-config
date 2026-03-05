9beda262677c19b5672c984067e297771b44c6d2
ayame113 (val440) <40050810+ayame113@users.noreply.github.com>
2026-03-05 20:40:34 +0900

    gitignoreルールを追加

--- src/eslint/gitignoreRuleSets.js
+++ src/eslint/gitignoreRuleSets.js
@@ -1,11 +1,20 @@
-// @ts-check
-
+import gitignore from "eslint-config-flat-gitignore";
 import { defineConfig, globalIgnores } from "eslint/config";
 
-export function gitignoreRuleSets() {
+/**
+ * @typedef GitignoreRuleSetOptions
+ * @property {string[]} [ignorePatterns] 追加で除外するファイルパターンの配列。デフォルトは`[]`。
+ */
+
+/**
+ * gitignoreを参照してファイルの除外設定を行うルール
+ * @param {GitignoreRuleSetOptions} [options] オプション
+ */
+export function gitignoreRuleSets(options = {}) {
     return defineConfig([
         globalIgnores([
-            "node_modules",
+            ...(options.ignorePatterns ?? []),
         ]),
+        gitignore(),
     ]);
 }
