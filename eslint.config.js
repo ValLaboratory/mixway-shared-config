4801b99fbda8410c99f6d935f1077ca7d9f5615e
ayame113 (val440) <40050810+ayame113@users.noreply.github.com>
2026-03-05 19:42:53 +0900

    設定を更新

--- eslint.config.js
+++ eslint.config.js
@@ -3,14 +3,5 @@ import { defineConfig } from "eslint/config";
 import { defaultRuleSets } from "./src/eslint/index.js";
 
 export default defineConfig([
-    defaultRuleSets({
-        languageOptions: {
-            parserOptions: {
-                tsconfigRootDir: import.meta.dirname,
-            },
-        },
-        typescriptResolverOptions: {
-            project: "./tsconfig.json",
-        },
-    }),
+    defaultRuleSets(),
 ]);
